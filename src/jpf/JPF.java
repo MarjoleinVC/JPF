01fe502f358f381b7a7ab82f3f7a58323ed750d9
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-17 16:05:07 +0200

    Aanpassingen 17/6

--- src/jpf/JPF.java
+++ src/jpf/JPF.java
@@ -76,6 +76,87 @@ public class JPF {
             grotere = null;
         }
 
-    }
+        //hoofdstuk 4.2.3: switch met breaks
+        {
+            int budget = 30;
 
+            String menu;
+
+            switch (budget) {
+                case 10:
+                    menu = "dagschotel";
+                    break;
+                case 20:
+                    menu = "weekendmenu";
+                    break;
+                case 30:
+                    menu = "zakenlunch";
+                    break;
+                case 40:
+                    menu = "fijnproeversmenu";
+                    break;
+                default:
+                    menu = "Verkeerde waarde";
+                    break;
+            }
+        }
+
+        //hoofdstuk 4.2.3: switch zonder breaks
+        //Hier wordt alles vanaf een waarde x verder uitgevoerd, dus voor €20 krijg je enkel hoofdgerecht, maar voor €50 krijg je alles.
+        {
+            int budget = 30;
+
+            String menu;
+
+            switch (budget) {
+                case 50:
+                    System.out.println("Champagne");
+                case 40:
+                    System.out.println("Voorgerecht");
+                case 30:
+                    System.out.println("Dessert en koffie");
+                case 20:
+                    System.out.println("Hoofdgerecht");
+            }
+        }
+
+        //hoofdstuk 4.3.4 breaks en labels
+        {
+            //declaratie en initialisatie van getallenreeks
+            byte[] getallen = {7, 12, 28, 2, 9};
+
+            //declaratie hulpvariabelen
+            int i = 0;
+            int faculteit;
+            long tussenresultaat;
+
+            //lus die de array doorloopt
+            while (i < getallen.length) {
+
+                //initialisatie hulpvariabelen
+                tussenresultaat = getallen[i];
+                int j = 1;
+
+                //berekenen faculteit
+                while (j < getallen[i]) {
+                    tussenresultaat *= j;
+                    if (tussenresultaat > Integer.MAX_VALUE) {
+                        break;
+                    }
+                    j++;
+                }
+
+                //uitvoer resultaat
+                if (j == getallen[i]) {
+                    faculteit = (int) tussenresultaat;
+                    System.out.println("De faculteit van " + getallen[i] + " is " + faculteit);
+                } else {
+                    System.out.println("De faculteit van " + getallen[i] + " is te groot voor een int.");
+                }
+
+                //teller verhogen
+                i++;
+            }
+        }
+    }
 }
