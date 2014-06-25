ef76d91284457b2b4a27881391820a23895398d9
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-25 11:46:37 +0200

    12.4.4 voorbeeld HashSet toevoegen

---
+++ src/Hoofstuk12/VoorbeeldHashSet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hoofstuk12;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author marjolein.vancelst
+ */
+public class VoorbeeldHashSet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Set op basis van HashSet");
+        Set hs = new HashSet();
+        vul(hs);
+        toon(hs);
+    }
+
+    private static void vul(Set s) {
+        s.add("fiets");
+        s.add("even");
+        s.add("dak");
+        s.add("citroen");
+        s.add("boom");
+        s.add("aap");
+    }
+
+    private static void toon(Set s) {
+        System.out.println();
+
+        for (Object obj : s) {
+            System.out.println(obj + "\t" + obj.hashCode());
+        }
+    }
+}
