36fbf5929879650f6f6c2eef9aa7e181c7eef2cd
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-25 14:33:02 +0200

    Toevoegen voorbeeld hoofdstuk 12.5 --> generic collection

--- src/Hoofdstuk12_5/VoorbeeldTypeCastenvsGenerics.java
+++ src/Hoofdstuk12_5/VoorbeeldTypeCastenvsGenerics.java
@@ -19,24 +19,21 @@ public class VoorbeeldTypeCastenvsGenerics {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Set coll = new HashSet();
+        Set<Cursus2> coll = new HashSet<Cursus2>();
 
         coll.add(new Cursus2(5, "Word", 100));
         coll.add(new Cursus2(3, "Excel", 110));
         coll.add(new Cursus2(1, "Windows", 90));
-        coll.add("test");
+        //delete: coll.add("test");
         coll.add(new Cursus2(4, "Access", 120));
         coll.add(new Cursus2(2, "PowerPoint", 80));
 
         int somPrijs = 0;
 
-        Iterator it = coll.iterator();
+        Iterator<Cursus2> it = coll.iterator();
         while (it.hasNext()) {
-            Object o = it.next();
-            if (o instanceof Cursus2) {
-                Cursus2 c = (Cursus2) o;
-                somPrijs += c.getPrijs();
-            }
+            Cursus2 c = it.next();
+            somPrijs += c.getPrijs();
         }
         System.out.println("Som van de prijzen: " + somPrijs);
     }
