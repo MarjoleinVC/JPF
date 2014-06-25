1b89e7977bfdc24965ead09130221176b1bc38d2
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-25 13:13:27 +0200

    12.4.6.2 voorbeeld oefening natural ordening - compareTo() --> aanpassen aan sorteren op cursusNaam

---
+++ src/Hoofdstuk12_4_6_2/Cursus.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hoofdstuk12_4_6_2;
+
+/**
+ *
+ * @author marjolein.vancelst
+ */
+public class Cursus implements Comparable {
+
+    private int cursusNr;
+    private String cursusNaam;
+    private int prijs;
+
+    public Cursus(int cursusNr, String cursusNaam, int prijs) {
+        setCursusNr(cursusNr);
+        setCursusNaam(cursusNaam);
+        this.prijs = prijs;
+    }
+
+    public int getCursusNr() {
+        return cursusNr;
+    }
+
+    public void setCursusNr(int nr) {
+        this.cursusNr = nr;
+    }
+
+    public String getCursusNaam() {
+        return cursusNaam;
+    }
+
+    public void setCursusNaam(String naam) {
+        this.cursusNaam = naam;
+    }
+
+    public int getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(int prijs) {
+        this.prijs = prijs;
+    }
+
+    @Override
+    public String toString() {
+        return (cursusNr + "\t" + cursusNaam + "\t" + prijs);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Cursus)) {
+            return false;
+        }
+        Cursus c = (Cursus) o;
+        return cursusNr == c.getCursusNr();
+    }
+
+    @Override
+    public int hashCode() {
+        return cursusNr;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        //sorteren op cursusnummer
+        Cursus c = (Cursus) o;
+        return cursusNaam.compareTo(c.getCursusNaam());
+    }
+}
