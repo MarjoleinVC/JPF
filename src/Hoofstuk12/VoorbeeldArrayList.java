ad24908bf12ab0d0c3f91f66163df14d3beb64fd
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-25 09:22:03 +0200

    Hoofdstuk 12.3.2.1 : uitbreiding met enkele extra methods van ListIterator

--- src/Hoofstuk12/VoorbeeldArrayList.java
+++ src/Hoofstuk12/VoorbeeldArrayList.java
@@ -8,6 +8,8 @@ package Hoofstuk12;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  *
@@ -20,11 +22,33 @@ public class VoorbeeldArrayList {
         List al = new ArrayList();
         vul(al);
         toon(al);
-        
+
+        System.out.println();
         System.out.println("Nieuwe elementen");
         al.add(3, "test");
         al.add("beer");
         toon(al);
+
+        //tweede String "ctiroen" vervangen door "tweede citroen"
+        System.out.println();
+        System.out.println("Element vervangen");
+        try {
+            ListIterator li = al.listIterator(6);
+            li.next();
+            li.set("tweede citroen");
+            li.add("kers");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("element niet gevonden (index out of bound): " + e.getMessage());
+        } catch (NoSuchElementException e) {
+            System.out.println("geen element beschikbaar: " + e.getMessage());
+        }
+
+        System.out.println();
+        System.out.print("ArrayList afgedrukt vanaf element ");
+        toonVanafIndex(al, 2);
+
+        System.out.print("\nArrayList omgekeerd afgedrukt");
+        toonVanafIndexOmgekeerd(al, al.size());
     }
 
     private static void vul(List lijst) {
@@ -61,3 +85,22 @@ public class VoorbeeldArrayList {
             System.out.println(woord);
         }
     }
+
+    private static void toonVanafIndex(List l, int startPos) {
+        System.out.println();
+        ListIterator li;
+
+        for (li = l.listIterator(startPos); li.hasNext();) {
+            System.out.println(li.next());
+        }
+    }
+
+    private static void toonVanafIndexOmgekeerd(List l, int startPos) {
+        System.out.println();
+        ListIterator li;
+
+        for (li = l.listIterator(startPos); li.hasPrevious();) {
+            System.out.println(li.previous());
+        }
+    }
+}
