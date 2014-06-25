9138c6b0e675bd9f70302c9c58e5b5d554b161c1
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-25 11:56:42 +0200

    12.4.5 voorbeeld LinkedHashSet toevoegen

---
+++ src/Hoofstuk12/VoorbeeldLinkedHashSet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hoofstuk12;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author marjolein.vancelst
+ */
+public class VoorbeeldLinkedHashSet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Set op basis van LinkedHashSet");
+        Set lhs = new LinkedHashSet();
+        vul(lhs);
+        toon(lhs);
+    }
+
+    private static void vul(Set s) {
+        s.add("fiets");
+        s.add("even");
+        s.add("dak");
+        s.add("citroen");
+        s.add("boom");
+        s.add("aap");
+    }
+
+    private static void toon(Set s) {
+        System.out.println();
+
+        for (Object obj : s) {
+            System.out.println(obj + "\t" + obj.hashCode());
+        }
+    }
+}
