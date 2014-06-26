5cb84f8ba2c810bfc1f1c408d0c637ab04795553
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-26 14:13:51 +0200

    Hoofdstuk 13 Byte streams --> gebruik van FileInputStream én BufferInputStream & FileOutputStream én BufferOutputStream:getallen worden eerst gebufferd totdat buffer vol is, daarna geschreven en vervolgens wordt weer nieuwe getallen gebufferd en weggeschreven naar bestand => sneller dan byte per byte wegschrijven

---
+++ src/Hoofdstuk13/ByteStreams.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hoofdstuk13;
+
+import java.io.*;
+
+/**
+ *
+ * @author marjolein.vancelst
+ */
+public class ByteStreams {
+
+    public static void main(String[] args) {
+
+        byte[] getallen = {2, 13, 14, 22, 29, 32, 17, 45, 44, 54, 4, 10, 99, 16, 26, 55, 77, 34, 51, 2, 41, 18, 20, 94, 8, 13, 42, 53, 66, 71, 82, 74, 36, 23, 21, 76, 16, 18, 48, 20, 3, 24, 3, 19, 60, 11, 65, 91, 88, 74, 56, 23, 12, 1, 10, 32, 49, 56, 17, 21};
+
+        //getallen naar bestand schrijven via buffer
+        FileOutputStream schrijfLijst = null;
+        BufferedOutputStream schrijfBuffer = null;
+
+        try {
+            //een nieuwe file en buffer aanmaken
+            schrijfLijst = new FileOutputStream("getallen.dat");
+            schrijfBuffer = new BufferedOutputStream(schrijfLijst);
+
+            //een nieuwe file (outputfile) aanmaken: true = bestaande file overschrijven:
+            //deLijst = new FileOutputStream("lotto.dat", true);
+            //alle getallen wegschrijven
+            for (byte getal : getallen) {
+                schrijfBuffer.write(getal);
+            }
+
+            //alle lottocijfers wegschrijven: kortere versie --> write()-method heeft variant met paramters (een array en twee integers)
+            //deLijst.write(lottocijfers, 0, lottocijfers.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            //de file sluiten
+            if (schrijfBuffer != null) {
+                try {
+                    schrijfBuffer.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+        System.out.println();
+
+        FileInputStream leesLijst = null;
+        BufferedInputStream leesBuffer = null;
+
+        //getallen inlezen vanaf bestand via buffer
+        try {
+            //een nieuwe file en buffer aanmaken
+            leesLijst = new FileInputStream("getallen.dat");
+            leesBuffer = new BufferedInputStream(leesLijst);
+
+            //alle getallen inlezen
+            int getal;
+            while ((getal = leesBuffer.read()) != -1) {
+                System.out.println(getal);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            //de file sluiten
+            if (leesBuffer != null) {
+                try {
+                    leesBuffer.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
