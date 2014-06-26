d231312c162ec0f46b694937a9f4525db9c25366
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-26 15:05:54 +0200

    Hoofdstuk 13 Character streams --> teken per teken inlezen in buffer als int --> toegevoegd; uitlezen door int te casten naar char

--- src/Hoofdstuk13/CharacterStreams.java
+++ src/Hoofdstuk13/CharacterStreams.java
@@ -10,20 +10,44 @@ import java.io.*;
 public class CharacterStreams {
 
     public static void main(String[] args) {
-        
-        FileReader file = null;
-        BufferedReader buffer = null;
+        FileWriter file = null;
+        BufferedWriter buffer = null;
+
+        //alle tekens van het alfabet wegschrijven
+        try {
+            //een nieuwe file aanmaken
+            file = new FileWriter("alfabet.txt");
+            buffer = new BufferedWriter(file);
+            buffer.write("Alle letters uit het alfabet");
+            buffer.newLine();
+            for (char letter = 'A'; letter <= 'Z'; letter++) {
+                buffer.write(letter);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (buffer != null) {
+                try {
+                    buffer.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+
+        FileReader fileLezer = null;
+        BufferedReader bufferLezer = null;
 
         //tekstbestand teken per teken inlezen en weergeven op het scherm
         try {
             //een nieuwe file aanmaken 
-            file = new FileReader("weerbericht.txt");
-            buffer = new BufferedReader(file);
+            fileLezer = new FileReader("alfabet.txt");
+            bufferLezer = new BufferedReader(fileLezer);
 
             //alle tekens inlezen tot aan eof 
             int gelezenTeken;
 
-            while ((gelezenTeken = buffer.read()) != -1) {
+            while ((gelezenTeken = bufferLezer.read()) != -1) {
                 System.out.print((char) gelezenTeken);
             }
             System.out.println();
