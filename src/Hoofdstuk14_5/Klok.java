d7c32aad889405d4eef0ce99a0f6d5486d6b2cc2
marjolein.vancelst <marjolein.vancelst@PAUL-EI1VSCGA7S.vdabheverlee.intra>
2014-06-27 09:56:08 +0200

    Hoofdstuk 14.5_6  De static method sleep van de class Thread + method interrupt van een Thread object

---
+++ src/Hoofdstuk14_5/Klok.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hoofdstuk14_5;
+
+import java.util.Date;
+
+/**
+ *
+ * @author marjolein.vancelst
+ */
+public class Klok implements Runnable {
+
+    @Override
+    public void run() {
+        boolean verderDoen = true;
+        while (verderDoen) {
+            System.out.println(new Date());
+            if (Thread.interrupted()) {
+                verderDoen = false; //klok stopzetten 
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.err.println(e);
+                verderDoen = false; //klok stopzetten
+            }
+        }
+    }
+}
